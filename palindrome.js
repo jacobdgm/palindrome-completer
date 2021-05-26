4dc598af96ea93a1d677dcce73e27e9f37d86aeb
4ristotle <jacob.4ristotle@gmail.com>
2021-05-26 11:46:14 -0400

    more robust filtering of non-alphabetic characters

--- palindrome.js
+++ palindrome.js
@@ -2,31 +2,29 @@
 
 function completePalindrome(){
 	document.getElementById("completionTextbox").value=
-	shortestPalindromicCompletion(document.getElementById("entryTextbox").value)
+	shortestPalindromicCompletion(document.getElementById("entryTextbox").value);
 }
 
 function reverseString(str){
-	return str.split("").reverse().join("")
+	return str.split('').reverse().join('');
 }
 
 function isPalindromic(str){
-	return reverseString(str) === str
+	return reverseString(str) === str;
 }
 
 function shortestPalindromicCompletion(str){
-	str = str.toLowerCase()
-	str = str.replace(/['"]/g,"")
-	str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
-	str = str.replace(/\s+/g,"")
-	strLength = str.length
-	tailIndex = 0
+	str = str.toLowerCase();
+	str = str.split('').filter(char => /[a-z]/.test(char)).join('');
+	strLength = str.length;
+	coreIndex = 0;
 	for (let i = 0; i < strLength; i++) {
-		sl = str.slice(i)
+		sl = str.slice(i);
 		if (isPalindromic(sl)) {
-			tailIndex = i
-			break
+			coreIndex = i;
+			break;
 		}
 	}
-	completion = str + reverseString(str.slice(0, tailIndex))
-	return completion
+	completion = str + reverseString(str.slice(0, coreIndex));
+	return completion;
 }
